9ef5ab73291b41533c9f26314b726c16c93d473d
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-19 23:36:39 +0530

    UC3 solved, part time wage is added

--- src/com/employee/oops/EmployeeWage.java
+++ src/com/employee/oops/EmployeeWage.java
@@ -5,17 +5,23 @@ package com.employee.oops;
 public class EmployeeWage {
 	//Constants
 	static final int FULL_TIME = 1;
+	static final int PART_TIME = 2;
 	static final int WAGE_PER_HR = 20;
 	
 	//method for checking employee attendance 
 	public static void attendance() {
 		//checking attendance using random function
-		int attendance = (int) Math.floor(Math.random() * 10 % 2);
+		int attendance = (int) Math.floor(Math.random() * 10 % 3);
 		int workingHrs = 0;
 		if(attendance == FULL_TIME) {
-			System.out.println("Employee is Present for day:");
+			System.out.println("Employee is Present for full day:");
 			workingHrs = 8;
 		}
+		else if(attendance == PART_TIME) {
+			System.out.println("Employee is Present for half day:");
+			workingHrs = 4;
+		}
+			
 		else {
 			System.out.println("Employee is Absent for day:");
 			workingHrs = 0;
