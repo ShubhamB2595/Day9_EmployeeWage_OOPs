ca8ddb6db1ebdb29fc42f25401c72096766cd71f
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-19 23:50:38 +0530

    UC4 solved, Calculated daily wage using switch case

--- src/com/employee/oops/EmployeeWage.java
+++ src/com/employee/oops/EmployeeWage.java
@@ -13,20 +13,24 @@ public class EmployeeWage {
 		//checking attendance using random function
 		int attendance = (int) Math.floor(Math.random() * 10 % 3);
 		int workingHrs = 0;
-		if(attendance == FULL_TIME) {
-			System.out.println("Employee is Present for full day:");
-			workingHrs = 8;
-		}
-		else if(attendance == PART_TIME) {
-			System.out.println("Employee is Present for half day:");
-			workingHrs = 4;
-		}
-			
-		else {
-			System.out.println("Employee is Absent for day:");
-			workingHrs = 0;
-		}
 		
+		switch(attendance) {
+			case FULL_TIME:
+				System.out.println("Employee is Present for full day:");
+				workingHrs = 8;
+				break;
+	
+			case PART_TIME:
+				System.out.println("Employee is Present for half day:");
+				workingHrs = 4;
+				break;
+				
+			default:
+				System.out.println("Employee is Absent for day:");
+				workingHrs = 0;	
+				break;
+		}
+
 		int dailyWage = workingHrs * WAGE_PER_HR;
 		System.out.println("Empoyee wage for the day is: " + dailyWage);
 	}
