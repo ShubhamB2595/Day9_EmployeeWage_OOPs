ffa451e74245261a85a4287ada173fe72108fed6
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-19 21:49:42 +0530

    Welcome to Employee Wage Computation Program

---
+++ src/com/employee/oops/EmployeeMain.java
@@ -0,0 +1,13 @@
+package com.employee.oops;
+/*
+ * main method for the employee wage computation
+ */
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
