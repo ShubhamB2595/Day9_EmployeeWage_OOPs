d75ef91e3b6cd5f5afbad6403401bca005fe6fd6
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-20 00:03:00 +0530

    UC5 solved, calculation of employee wage for a month is done

--- src/com/employee/oops/EmployeeWage.java
+++ src/com/employee/oops/EmployeeWage.java
@@ -7,31 +7,32 @@ public class EmployeeWage {
 	static final int FULL_TIME = 1;
 	static final int PART_TIME = 2;
 	static final int WAGE_PER_HR = 20;
+	static final int MAX_WORK_DAYS = 20;
 	
 	//method for checking employee attendance 
 	public static void attendance() {
-		//checking attendance using random function
-		int attendance = (int) Math.floor(Math.random() * 10 % 3);
 		int workingHrs = 0;
 		
-		switch(attendance) {
-			case FULL_TIME:
-				System.out.println("Employee is Present for full day:");
-				workingHrs = 8;
-				break;
+		for(int i =0; i<MAX_WORK_DAYS; i++) {
+		//checking attendance using random function
+			int attendance = (int) Math.floor(Math.random() * 10 % 3);
+		
+			switch(attendance) {
+				case FULL_TIME:
+					workingHrs += 8;
+					break;
 	
-			case PART_TIME:
-				System.out.println("Employee is Present for half day:");
-				workingHrs = 4;
-				break;
+				case PART_TIME:
+					workingHrs += 4;
+					break;
 				
-			default:
-				System.out.println("Employee is Absent for day:");
-				workingHrs = 0;	
-				break;
-		}
+				default:
+					workingHrs += 0;	
+					break;
+			}
 
-		int dailyWage = workingHrs * WAGE_PER_HR;
-		System.out.println("Empoyee wage for the day is: " + dailyWage);
+		}
+		int wage = workingHrs * WAGE_PER_HR;
+		System.out.println("Empoyee wage for the month is: " + wage);
 	}
 }
