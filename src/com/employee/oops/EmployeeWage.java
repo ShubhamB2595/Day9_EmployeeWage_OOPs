e4c79c1a35d8f2442c3def5d143908523eb3f9b7
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-19 23:30:32 +0530

    Uc2 solved, calculation for daily wage is added

--- src/com/employee/oops/EmployeeWage.java
+++ src/com/employee/oops/EmployeeWage.java
@@ -5,16 +5,23 @@ package com.employee.oops;
 public class EmployeeWage {
 	//Constants
 	static final int FULL_TIME = 1;
+	static final int WAGE_PER_HR = 20;
 	
 	//method for checking employee attendance 
 	public static void attendance() {
 		//checking attendance using random function
 		int attendance = (int) Math.floor(Math.random() * 10 % 2);
+		int workingHrs = 0;
 		if(attendance == FULL_TIME) {
 			System.out.println("Employee is Present for day:");
+			workingHrs = 8;
 		}
 		else {
 			System.out.println("Employee is Absent for day:");
+			workingHrs = 0;
 		}
+		
+		int dailyWage = workingHrs * WAGE_PER_HR;
+		System.out.println("Empoyee wage for the day is: " + dailyWage);
 	}
 }
